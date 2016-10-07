b9319333e2dbec09ea6d83b96bc5ca98486f4e85
Ashish Ahuja <ahj.ashish@gmail.com>
2016-10-08 00:20:03 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -26,20 +26,20 @@ module.exports = function (config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress','coverage'],
+        // reporters: ['progress','coverage'],
 
         coverageReporter: {
             type : 'html',
             dir : 'coverage/'
         },
         
-        singleRun = true;
+        singleRun : true,
         
-        reporters = ['dots', 'junit'];
+        reporters : ['dots', 'junit'],
         
-        junitReporter = {
+        junitReporter : {
             outputFile: 'test-results.xml'
-        };
+        },
         // web server port
         port: 9876
 
